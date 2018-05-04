b7667abea700770111cfc9f8387d2074dcccc213
morteza <ma.rvgroup@gmail.com>
2018-05-04 21:12:06 +0430

    code first sequelize

--- bug-restful-api/src/data-access/postgres-main-query.sql
+++ bug-restful-api/src/data-access/postgres-main-query.sql
@@ -1,6 +1,14 @@
 ﻿
 --  	BT_System (Bug Tracking System)
 --**************************************************************************************
+ -- EXTENSION
+ 
+ CREATE EXTENSION IF NOT EXISTS  pgcrypto;
+ CREATE EXTENSION IF NOT EXISTS  plpgsql;
+ CREATE EXTENSION IF NOT EXISTS  "uuid-ossp";
+
+ --**************************************************************************************
+ 
 CREATE OR REPLACE FUNCTION public.hash_password()
   RETURNS trigger AS
 $BODY$
@@ -14,28 +22,25 @@ $BODY$
   COST 100;
 ALTER FUNCTION public.hash_password()
   OWNER TO morteza;
+  
 --**************************
+
 DROP TRIGGER IF EXISTS hash_and_format_account ON public.account;
 CREATE TRIGGER hash_and_format_account
   BEFORE INSERT
   ON public.account
   FOR EACH ROW
   EXECUTE PROCEDURE public.hash_password();
---**************************************************************************************
-
---**************************************************************************************
- -- EXTENSION
- CREATE EXTENSION IF NOT EXISTS  pgcrypto;
- CREATE EXTENSION IF NOT EXISTS  plpgsql;
- CREATE EXTENSION IF NOT EXISTS  "uuid-ossp";
-
+  
  --**************************************************************************************
 --SEQUENCES
+
 CREATE SEQUENCE IF NOT EXISTS task_global_seq
 
 
  --**************************************************************************************
  ------------------------------------- S E E D S ----------------------------------------
+ 
 INSERT INTO public.op_status (op_status_id,"name") VALUES (0,'default'),(1,'active'),(2,'removed');
 INSERT INTO public.op_flag   (op_flag_id,  "name") VALUES (0,'none'),(1,'warn'),(2,'panic');
 INSERT INTO public.op_role   (op_role_id,  "name") VALUES (0,'root'),(1,'admin'),(2,'manager'),(3,'client');
